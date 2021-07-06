474fa77390384272304892a222a9b8516394bf4f
Jacob Piland <piland.jacob@gmail.com>
2021-07-05 22:47:38 -0500

    add disc database

---
+++ db/migrate/20210706034617_create_discs.rb
@@ -0,0 +1,13 @@
+class CreateDiscs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :discs do |t|
+      t.string :name
+      t.boolean :in_production
+      t.integer :speed
+      t.timestamp :created_at
+      t.timestamp :updated_at
+      t.integer :manufacturer_id
+      t.references :disc_manufacturer, foreign_key: true
+    end
+  end
+end
