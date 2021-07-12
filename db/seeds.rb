43777af09aeb04dfaedf91a2e6e22171859451b5
Jacob Piland <piland.jacob@gmail.com>
2021-07-11 19:04:42 -0500

    change data from manual input to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Disc.destroy_all
+DiscsManufacturer.destroy_all
+
+@dm1 = DiscsManufacturer.create!(name:'Innova', location:'Ontario,California', international: true, variety_of_discs: 90)
+@dm2 = DiscsManufacturer.create!(name:'Discraft', location:'London, Ontario', international: true, variety_of_discs: 48)
+@dm3 = DiscsManufacturer.create!(name:'Mint', location:'Austin, Texas', international: false, variety_of_discs: 7)
+
+@disc1 = @dm1.discs.create!(name: 'Destroyer', in_production: true, speed: 12)
+@disc2 = @dm1.discs.create!(name: 'TeeBird', in_production: true, speed: 7)
+@disc3 = @dm2.discs.create!(name: 'Challenger', in_production: true, speed: 12)
+@disc4 = @dm3.discs.create!(name: 'Freetail', in_production: true, speed: 10)
+@disc5 = @dm2.discs.create!(name: 'Flash', in_production: false, speed: 11)
